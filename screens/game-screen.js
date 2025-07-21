ff6423e3429c71ab187a452d6397e07ec978af8f
Amr Elshemy <amrrmohamed87@gmail.com>
2025-07-22 01:01:43 +0300

    add GameScreen component to handle game logic and display opponent's guess

---
+++ screens/game-screen.js
@@ -0,0 +1,47 @@
+import { Text, View, StyleSheet } from "react-native";
+import Title from "../components/ui/title";
+import { useState } from "react";
+import NumberContainer from "../components/game/number-container";
+
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+function GameScreen(props) {
+  const initialGuess = generateRandomBetween(1, 100, props.userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <Text>Higher or Lower?</Text>
+      </View>
+      <View></View>
+    </View>
+  );
+}
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#ddb52f",
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: "#ddb52f",
+    padding: 12,
+  },
+});
